4b8c75c59eb9ba12c8279178cfa430d6c6e756a5
alexandra <alexaspalato@gmail.com>
2019-04-12 08:20:12 +0200

    remove unused elements, rename components, create pages about and contact

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/Layout"
+import SEO from "../components/Seo"
+
+const AboutPage = () => (
+  <Layout>
+    <SEO title="Contact Page" />
+    <h1>Contact Page</h1>
+  </Layout>
+)
+
+export default AboutPage
---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/Layout"
+import SEO from "../components/Seo"
+
+const ContactPage = () => (
+  <Layout>
+    <SEO title="Contact page" />
+    <h1>Contact Page</h1>
+  </Layout>
+)
+
+export default ContactPage
