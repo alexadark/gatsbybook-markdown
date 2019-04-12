a709a268ba39c69dbb21130036b782f52fa02978
alexandra <alexaspalato@gmail.com>
2019-04-12 08:32:44 +0200

    add content to pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,6 +7,28 @@ const AboutPage = () => (
   <Layout>
     <SEO title="Contact Page" />
     <h1>About Page</h1>
+    <p>
+      Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac
+      cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo
+      sit amet risus. Cras mattis consectetur purus sit amet fermentum. Morbi
+      leo risus, porta ac consectetur ac, vestibulum at eros. Cum sociis natoque
+      penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis
+      mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia
+      odio sem nec elit. Cum sociis natoque penatibus et magnis dis parturient
+      montes, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare
+      sem lacinia quam venenatis vestibulum. Donec sed odio dui. Praesent
+      commodo cursus magna, vel scelerisque nisl consectetur et. Nullam quis
+      risus eget urna mollis ornare vel eu leo. Nullam id dolor id nibh
+      ultricies vehicula ut id elit. Nullam quis risus eget urna mollis ornare
+      vel eu leo. Curabitur blandit tempus porttitor. Aenean lacinia bibendum
+      nulla sed consectetur. Duis mollis, est non commodo luctus, nisi erat
+      porttitor ligula, eget lacinia odio sem nec elit. Praesent commodo cursus
+      magna, vel scelerisque nisl consectetur et. Cras mattis consectetur purus
+      sit amet fermentum. Nullam id dolor id nibh ultricies vehicula ut id elit.
+      Donec ullamcorper nulla non metus auctor fringilla. Aenean lacinia
+      bibendum nulla sed consectetur. Cum sociis natoque penatibus et magnis dis
+      parturient montes, nascetur ridiculus mus.
+    </p>
   </Layout>
 )
 
